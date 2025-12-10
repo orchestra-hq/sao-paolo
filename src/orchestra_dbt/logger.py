cff1550c62c99f374baffd62943f40dbda860297
Oliver Crawford <oliver@getorchestra.io>
2025-12-10 11:33:09 +0000

    fix error logging

--- src/orchestra_dbt/logger.py
+++ src/orchestra_dbt/logger.py
@@ -8,7 +8,7 @@ from .models import ModelNode
 
 
 def _log(msg: str, fg: str | None, error: bool = False) -> None:
-    text = str(datetime.now().strftime("%H:%M:%S"))
+    text = f"[{str(datetime.now().strftime('%H:%M:%S'))}]"
     if error:
         text += " [ERROR]"
     text += f" [{SERVICE_NAME}] {msg}"
