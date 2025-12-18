388700d2a95eed3804d62e6eddf6ba6895cd03b6
Oliver Crawford <oliver@getorchestra.io>
2025-12-18 16:12:00 +0000

    add support for snowflake

--- example_package/models/my_utils/date_dimension.sql
+++ example_package/models/my_utils/date_dimension.sql
@@ -9,6 +9,7 @@ select
     quarter (date_day) as quarter
 from
     (
+        {% if target.type == 'databricks' %}
         select
             explode (
                 sequence (
@@ -17,4 +18,20 @@ from
                     interval 1 day
                 )
             ) as date_day
+        {% elif target.type == 'snowflake' %}
+        select
+            dateadd(day, seq4(), '2020-01-01'::date) as date_day
+        from
+            table(generator(rowcount => 3653))
+        where
+            date_day <= '2029-12-31'::date
+        {% else %}
+        -- Default to Snowflake syntax for other adapters
+        select
+            dateadd(day, seq4(), '2020-01-01'::date) as date_day
+        from
+            table(generator(rowcount => 3653))
+        where
+            date_day <= '2029-12-31'::date
+        {% endif %}
     )
