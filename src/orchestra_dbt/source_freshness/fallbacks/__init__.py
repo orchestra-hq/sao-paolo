f32d72ac0cb9d0ef1a80eb0ffebbcb6f165ab480
Oliver Crawford <oliver@getorchestra.io>
2026-04-21 17:41:59 +0100

    refactor the source freshness code to allow for contribs in the future

---
+++ src/orchestra_dbt/source_freshness/fallbacks/__init__.py
@@ -0,0 +1,11 @@
+from .registry import (
+    FALLBACK_BY_ADAPTER_TYPE,
+    loaded_at_fields_unset,
+    try_registered_fallback,
+)
+
+__all__ = [
+    "FALLBACK_BY_ADAPTER_TYPE",
+    "loaded_at_fields_unset",
+    "try_registered_fallback",
+]
